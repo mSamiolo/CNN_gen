f93955b09c13e24c0948c2fa764e21e91c1c9830
Mattia Samiolo <m.samiolo@brambleenergy.com>
2022-07-03 11:25:23 +0100

    Setting percetpron as a module

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,11 @@
-struct Perceptron {
-    weight: Vec<f64>,
-    bias: f64,
-}
-
-impl Perceptron {
-    // Setting Weights
-    
-    fn new(inputs: usize, b: f64 ) -> Self {
-        Self { 
-            weight: vec![0.0;inputs], 
-            bias: b
-        }
-    }
-
-    fn set_weights(&mut self, w_init: f64) {
-       self.weight.fill(w_init);
-    }
-
-    // Triggering behaviour
-    fn sigmoid(&mut self, x: f64) -> f64 {
-        1.0/(1.0 + (-x).exp())
-    }
-
-    fn run(&mut self, x: Vec<f64>) -> f64 {
-        // inizialization node_value
-        let mut node_value = 0.0;
-        // weighted summatory of inlets 
-        for i in 0..self.weight.len() as usize {
-            node_value += self.weight[i] * x[i];
-        } 
-        node_value += self.bias;
-        self.sigmoid(node_value) 
-    }
-}
-
+mod perceptron;
+use perceptron::Perceptron;
 
 fn main() {
-    let mut and_gate = Perceptron::new(2, 60.0);
-    and_gate.set_weights(50.0);
+    let mut and_gate = Perceptron::new(2, -10.0);
+    and_gate.set_weights(20.0);
     let result = and_gate.run(vec![0.0, 0.0]);
-    println!("Outlet {:?}:", result);
+    println!("Outlet {:?}:", result.round());
 }
 
 // ----------------------------------------------------------------------- //
---
+++ src/perceptron/mod.rs
@@ -0,0 +1,35 @@
+pub struct Perceptron {
+    weight: Vec<f64>,
+    bias: f64,
+}
+
+impl Perceptron {
+    // Setting Weights
+    
+    pub fn new(inputs: usize, b: f64 ) -> Self {
+        Self { 
+            weight: vec![0.0;inputs], 
+            bias: b
+        }
+    }
+
+    pub fn set_weights(&mut self, w_init: f64) {
+       self.weight.fill(w_init);
+    }
+
+    // Triggering behaviour
+    pub fn sigmoid(&mut self, x: f64) -> f64 {
+        1.0/(1.0 + (-x).exp())
+    }
+
+    pub fn run(&mut self, x: Vec<f64>) -> f64 {
+        // inizialization node_value
+        let mut node_value = 0.0;
+        // weighted summatory of inlets 
+        for i in 0..self.weight.len() as usize {
+            node_value += self.weight[i] * x[i];
+        } 
+        node_value += self.bias;
+        self.sigmoid(node_value) 
+    }
+}
