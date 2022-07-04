15ec4c7e04bd58e87984a703c49cd070d73635af
Mattia Samiolo <m.samiolo@brambleenergy.com>
2022-07-04 21:28:45 +0100

    Vector on vector applied

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+// Shortening the module name
 mod perceptron;
 use perceptron::Perceptron;
 
-fn main() {
+fn run_perceptron() {
     let mut and_gate = Perceptron::new(2, -10.0);
     and_gate.set_weights(20.0);
     let result = and_gate.run(vec![0.0, 0.0]);
@@ -10,24 +11,45 @@ fn main() {
 
 // ----------------------------------------------------------------------- //
 
-// struct MultiLayerPercetron {
-//     bias: f64, // The bias term. The same bias is used for all neurons.
-//     eta: f64,  // Learning rate 
-//     //layers: Vec<usize>
-// }
+#[allow(dead_code, unused)]
+struct MultiLayerPercetron {
+    bias: f64,          // The bias term. The same bias is used for all neurons.
+    eta: f64,           // Learning rate
+    layers: Vec<usize>, // Array with the number of elements per layer
+    // values: Matrix,
+    // d: Matrix,
+    // network: Matrix
+    values: Vec<Vec<f64>>,
+    d: Vec<Vec<f64>>,
+    network: Vec<Vec<Perceptron>>,
+}
 
-// impl Default for MultiLayerPercetron {
-    
-//     fn default() -> Self {
-//         Self {
-//             bias: 0.0, 
-//             eta: 0.0,
-//         }
-//     }
+impl MultiLayerPercetron {
+    fn new(layers: Vec<usize>, bias: f64, eta: f64) -> Self {
+        // Initialization vector parameters
+        let mut values = Vec::new();
+        let mut d = Vec::new(); //  The list of lists of error terms (d = lowercase deltas)
+        let mut network = Vec::new();
 
-// impl MultiLayerPercetron  {
-// 	fn set_layers(&self, x: [f64]) {
-// 		let &mut layers = x;
-// 	}
-// }	
-// }
+        // let mut neuron = Perceptron { weight };
+
+        for i in 0..layers.len() {
+            values[i] = vec![0.0; layers[i]];
+            d[i] = vec![0.0; layers[i]];
+            if i > 0 {
+                network[i] = vec![ Perceptron { vec![0.0, layers[i-1],  bias]} ; layers[i] ]
+            }
+        }
+        // Initialization Multipercetron parameters
+        Self {
+            bias,
+            eta,
+            layers,
+            values,
+            d,
+            network,
+        }
+    }
+}
+
+fn main() {}
--- src/perceptron/mod.rs
+++ src/perceptron/mod.rs
@@ -5,31 +5,30 @@ pub struct Perceptron {
 
 impl Perceptron {
     // Setting Weights
-    
-    pub fn new(inputs: usize, b: f64 ) -> Self {
-        Self { 
-            weight: vec![0.0;inputs], 
-            bias: b
+    pub fn new(inputs: usize, b: f64) -> Self {
+        Self {
+            weight: vec![0.0; inputs],
+            bias: b,
         }
     }
 
     pub fn set_weights(&mut self, w_init: f64) {
-       self.weight.fill(w_init);
+        self.weight.fill(w_init);
     }
 
     // Triggering behaviour
     pub fn sigmoid(&mut self, x: f64) -> f64 {
-        1.0/(1.0 + (-x).exp())
+        1.0 / (1.0 + (-x).exp())
     }
 
     pub fn run(&mut self, x: Vec<f64>) -> f64 {
         // inizialization node_value
         let mut node_value = 0.0;
-        // weighted summatory of inlets 
+        // weighted summatory of inlets
         for i in 0..self.weight.len() as usize {
             node_value += self.weight[i] * x[i];
-        } 
+        }
         node_value += self.bias;
-        self.sigmoid(node_value) 
+        self.sigmoid(node_value)
     }
 }
